8827685c60ddf8cdc3b567a4287800b6cd3d194b
Slim-Hady <mohamed.elsogher.2024@aiu.edu.eg>
2026-04-29 03:55:58 +0300

    feat: Add user schema and database connection

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import 'dotenv/config';
 import {app} from './app';
-
+import mongoose from 'mongoose';
 const PORT = process.env.PORT || 3001;
+const DB = process.env.DATABASE;
+
+mongoose.connect(DB!)
+.then(() => console.log("Connect with database"))
+.catch((err) => console.log(err));
 
 app.listen(PORT , () => {
     return console.log(`App is running on Port ${PORT}`);
