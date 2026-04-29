4a131abe250d20553da0a180d382a4e00d570399
Slim-Hady <mohamed.elsogher.2024@aiu.edu.eg>
2026-04-29 03:19:06 +0300

    init: add project skeleton

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
 
+import morgan from 'morgan';
+
 export const app = express();
 
+app.use(morgan('dev'));
+app.use(express.json());
+app.set('query parser', 'extended');
 
 app.get('/' , (req , res) => {
     res.send("Hello From the server");
